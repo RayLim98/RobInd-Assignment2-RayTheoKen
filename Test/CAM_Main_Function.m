63378e1b28d1b6260269bb96fbc949de47dac247
TheodoreG19 <72005531+TheodoreG19@users.noreply.github.com>
2022-05-05 14:17:11 +1000

    Added RectangularPrism Class File

--- Test/CAM_Main_Function.m
+++ Test/CAM_Main_Function.m
@@ -16,33 +16,103 @@ robot.plot(q,'workspace',workspace,'scale',scale);                  % Plot the r
 
 %% Create Light Curtain Wall
 
-wall = LightCurtainWall(1, 2, [0 1.9 -0.5]);
-hold on
+% % % wall = LightCurtainWall(1, 2, [0 1.9 -0.5]);
+% % % hold on
 
 
 %% Creating a cube and use teach to see collision detection
-centerpnt = [2,0,-0.5];
+centerpnt = [2,0,0.05];
 side = 1.5;
 plotOptions.plotFaces = true;
 [vertex,faces,faceNormals] = RectangularPrism(centerpnt-side/2, centerpnt+side/2,plotOptions);
 axis equal
 camlight
-
-    pPoints = [1.25,0,-0.5 ...
-        ;2,0.75,-0.5 ...
-        ;2,-0.75,-0.5 ...
-        ;2.75,0,-0.5];
-    pNormals = [-1,0,0 ...
-            ; 0,1,0 ...
-            ; 0,-1,0 ...
-            ;1,0,0];
-
+% pPoints = [1.25,0,-0.5 ...
+%         ;2,0.75,-0.5 ...
+%         ;2,-0.75,-0.5 ...
+%         ;2.75,0,-0.5];
+% pNormals = [-1,0,0 ...
+%             ; 0,1,0 ...
+%             ; 0,-1,0 ...
+%             ;1,0,0];
 robot.teach;
+
 hold on
 
 
-%% IsCollision
+% 2.4: Get the transform of every joint (i.e. start and end of every link)
+tr = zeros(4,4,robot.n+1);
+tr(:,:,1) = robot.base;
+L = robot.links;
+for i = 1 : robot.n
+    tr(:,:,i+1) = tr(:,:,i) * trotz(q(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
+end
 
+% 2.5: Go through each link and also each triangle face
+for i = 1 : size(tr,3)-1    
+    for faceIndex = 1:size(faces,1)
+        vertOnPlane = vertex(faces(faceIndex,1)',:);
+        [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
+        if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
+            plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
+            display('Intersection');
+        end
+    end    
+end
+
+% 2.6: Go through until there are no step sizes larger than 1 degree
+q1 = [-pi/4,0,0];
+q2 = [pi/4,0,0];
+steps = 20;
+while ~isempty(find(1 < abs(diff(rad2deg(jtraj(q1,q2,steps)))),1))
+    steps = steps + 1;
+end
+qMatrix = jtraj(q1,q2,steps);
+
+% 2.7
+result = true(steps,1);
+for i = 1: steps
+    result(i) = IsCollision(robot,qMatrix(i,:),faces,vertex,faceNormals,false);
+    robot.animate(qMatrix(i,:));
+end
+
+%% IsIntersectionPointInsideTriangle
+% Given a point which is known to be on the same plane as the triangle
+% determine if the point is 
+% inside (result == 1) or 
+% outside a triangle (result ==0 )
+function result = IsIntersectionPointInsideTriangle(intersectP,triangleVerts)
+
+u = triangleVerts(2,:) - triangleVerts(1,:);
+v = triangleVerts(3,:) - triangleVerts(1,:);
+
+uu = dot(u,u);
+uv = dot(u,v);
+vv = dot(v,v);
+
+w = intersectP - triangleVerts(1,:);
+wu = dot(w,u);
+wv = dot(w,v);
+
+D = uv * uv - uu * vv;
+
+% Get and test parametric coords (s and t)
+s = (uv * wv - vv * wu) / D;
+if (s < 0.0 || s > 1.0)        % intersectP is outside Triangle
+    result = 0;
+    return;
+end
+
+t = (uv * wu - uu * wv) / D;
+if (t < 0.0 || (s + t) > 1.0)  % intersectP is outside Triangle
+    result = 0;
+    return;
+end
+
+result = 1;                      % intersectP is in Triangle
+end
+
+%% IsCollision
 % Given a robot model (robot), and trajectory (i.e. joint state vector) (qMatrix)
 % and triangle obstacles in the environment (faces,vertex,faceNormals)
 function result = IsCollision(robot,qMatrix,faces,vertex,faceNormals,returnOnceFound)
@@ -73,48 +143,64 @@ for qIndex = 1:size(qMatrix,1)
 end
 end
 
+%% GetLinkPoses
+% q - robot joint angles
+% robot -  seriallink robot model
+% transforms - list of transforms
+function [ transforms ] = GetLinkPoses( q, robot)
 
+links = robot.links;
+transforms = zeros(4, 4, length(links) + 1);
+transforms(:,:,1) = robot.base;
 
-
-% % % % Get the transform of every joint (i.e. start and end of every link)
-% % % tr = zeros(4,4,robot.n+1);
-% % % tr(:,:,1) = robot.base;
-% % % L = robot.links;
-% % % for i = 1 : robot.n
-% % %     tr(:,:,i+1) = tr(:,:,i) * trotz(q(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
-% % % end
-% % % 
-% % % %Go through each link and each triangle face
-% % % for i = 1 : size(tr,2)-1    
-% % %     for faceIndex = 1:size(faces,1)
-% % %         vertOnPlane = vertex(faces(faceIndex,1)',:);
-% % %         [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
-% % %         if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
-% % %             plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
-% % %             display('Intersection');
-% % %         end
-% % %     end    
-% % % end
-% % % 
-% % % %Go through each steps 
-% % % q1 = [-pi/4,0,0];
-% % % q2 = [pi/4,0,0];
-% % % steps = 10;
-% % % while ~isempty(find(1 < abs(diff(rad2deg(jtraj(q1,q2,steps)))),1))
-% % %     steps = steps + 1;
-% % % end
-% % % qMatrix = jtraj(q1,q2,steps);
-
-
-%% Confirm Collision
-
-%check each joint states in the trajectory to work out which ones are in
-%collisions
-% 0 = no collision
-% 1 = yes collision (unsafe)
-
-result = true(steps,1)
-for i = 1:steps
-    result(i) = CollisionCheck(robot,q1,q2);
+for i = 1:length(links)
+    L = links(1,i);
+    
+    current_transform = transforms(:,:, i);
+    
+    current_transform = current_transform * trotz(q(1,i) + L.offset) * ...
+    transl(0,0, L.d) * transl(L.a,0,0) * trotx(L.alpha);
+    transforms(:,:,i + 1) = current_transform;
 end
+end
+
+%% FineInterpolation
+% Use results from Q2.6 to keep calling jtraj until all step sizes are
+% smaller than a given max steps size
+function qMatrix = FineInterpolation(q1,q2,maxStepRadians)
+if nargin < 3
+    maxStepRadians = deg2rad(1);
+end
+    
+steps = 2;
+while ~isempty(find(maxStepRadians < abs(diff(jtraj(q1,q2,steps))),1))
+    steps = steps + 1;
+end
+qMatrix = jtraj(q1,q2,steps);
+end
+
+%% InterpolateWaypointRadians
+% Given a set of waypoints, finely intepolate them
+function qMatrix = InterpolateWaypointRadians(waypointRadians,maxStepRadians)
+if nargin < 2
+    maxStepRadians = deg2rad(1);
+end
+
+qMatrix = [];
+for i = 1: size(waypointRadians,1)-1
+    qMatrix = [qMatrix ; FineInterpolation(waypointRadians(i,:),waypointRadians(i+1,:),maxStepRadians)]; %#ok<AGROW>
+end
+end
+
+% % % %% Confirm Collision
+% % % 
+% % % %check each joint states in the trajectory to work out which ones are in
+% % % %collisions
+% % % % 0 = no collision
+% % % % 1 = yes collision (unsafe)
+% % % 
+% % % result = true(steps,1)
+% % % for i = 1:steps
+% % %     result(i) = CollisionCheck(robot,q1,q2);
+% % % end
 
--- Test/Cabinet_Code.m
+++ Test/Cabinet_Code.m
@@ -1,6 +1,6 @@
-
+%% Robotics
 % Lab 5 - Questions 2 and 3: 3-link plannar collision check and avoidance
-function [ CheckCollission ] = Lab5Solution_Question2and3( )
+function [  ] = Lab5Solution_Question2and3( )
 
 % clf
 close all;
@@ -15,11 +15,11 @@ L3 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi]);
 robot = SerialLink([L1 L2 L3 ],'name','myRobot');                     
 q = zeros(1,3);                                                     % Create a vector of initial joint angles        
 scale = 0.5;
-workspace = [-2 2 -2 2 -0.05 2];                                       % Set the size of the workspace when drawing the robot
+workspace = [-2 2 -2 2 -0.2 2];                                       % Set the size of the workspace when drawing the robot
 robot.plot(q,'workspace',workspace,'scale',scale);                  % Plot the robot
 
 % 2.2 and 2.3
-centerpnt = [2,0,-0.5];
+centerpnt = [2,0,0.05];
 side = 1.5;
 plotOptions.plotFaces = true;
 [vertex,faces,faceNormals] = RectangularPrism(centerpnt-side/2, centerpnt+side/2,plotOptions);
@@ -58,7 +58,7 @@ end
 % 2.6: Go through until there are no step sizes larger than 1 degree
 q1 = [-pi/4,0,0];
 q2 = [pi/4,0,0];
-steps = 2;
+steps = 20;
 while ~isempty(find(1 < abs(diff(rad2deg(jtraj(q1,q2,steps)))),1))
     steps = steps + 1;
 end
@@ -72,105 +72,43 @@ for i = 1: steps
 end
 
 
-
-
-
-
-
-
-
-
-
-
-% % % % % bot = Sawyer(0,0,0);
-% % % % % bot.model.teach();
-% % % % % hold on
-% % % % % 
-% % % % % %% Initialise Robot 
-% % % % % % 2.1: Make Sawyer model 
-% % % % % L1 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi]);
-% % % % % L2 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi]);
-% % % % % L3 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi]); 
-% % % % % % L4 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi]); 
-% % % % % % L5 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi]); 
-% % % % % % L6 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi]); 
-% % % % % robot = SerialLink([L1 L2 L3 L4 L5 L6],'name','Robot');                     
-% % % % % q = zeros(1,6);                                                     % Create a vector of initial joint angles        
-% % % % % scale = 0.5;
-% % % % % workspace = [-2 2 -2 2 -0.05 2];                                       % Set the size of the workspace when drawing the robot
-% % % % % robot.plot(q,'workspace',workspace,'scale',scale);                  % Plot the robot
-% % % % %         
-% % % % % %% Class for Workspace Interaction as an Object
-% % % % %         classdef  WorkspaceObjectClass
-% % % % %             properties
-% % % % % %            assign x of mesh
-% % % % % %            assign y of mesh
-% % % % % %            assign z of mesh
-% % % % % %            assign Pp arbitrary of plane for detection (check line method)
-% % % % % %   Wall_ = newWall_1;
-% % % % % %     Wall_2 = newWall_2(X,Y);
-% % % % % %       Wall_3 = newWall_3(X,Y);
-% % % % % %         Wall_4 = newWall_4(X,Y);
-% % % % %             end      
-% % % % %             
-% % % % %          
-% % % % % %% CAM Object Class
-% % % % % 
-% % % % % shopWallIntersect = shopWallInt;
-% % % % % 
-% % % % % q; %joint states
-% % % % % 
-% % % % % % % %% Robot set up robot
-% % % % % % % q0 = bot.model.getpos();
-% % % % % % % qr = [0 -pi -pi/2 pi/2 0 -pi/2 0];
-% % % % % % % 
-% % % % % % % qM = jtraj(q0,qr,30);
-% % % % % % % 
-% % % % % % % bot.model.plot(qM);
-% % % % % % % q0 = bot.model.getpos();
-% % % % % % % bot.model.teach();
-% % % % % 
-% % % % % %% Test to see line collision with wall 
-% % % % % % [This section will return with a boolean expression if there is a
-% % % % % % collision with end-effector]
-% % % % % 
-% % % % % % (forward kinematic for for each joint)
-% % % % % % From Lab 5 
-% % % % % % % % % 2.4: Get the transform of every joint (i.e. start and end of every link)
-% % % % % % % % tr = zeros(4,4,robot.n+1);
-% % % % % % % % tr(:,:,1) = robot.base;
-% % % % % % % % L = robot.links;
-% % % % % % % % for i = 1 : robot.n
-% % % % % % % %     tr(:,:,i+1) = tr(:,:,i) * trotz(q(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
-% % % % % % % % end
-% % % % % 
-% % % % % function [tr] = GetLinkPointMatrixes(robot)
-% % % % % %GETLINKLINEMATRIXS 
-% % % % %     tr(:,:,:,1) = robot.base;
-% % % % %     for i = 1: robot.n
-% % % % %         link = robot.links(i);
-% % % % %         tr(:,:,:, i+1) = tr(:,:,:,i) * trotz(q(i)+link.offset) * transl(0,0,link.d) * transl(link.a,0,0) * trotx(link.alpha);
-% % % % %     end
-% % % % % end
-% % % % % 
-% % % % % 
-% % % % % %Tr(1-7) for 6 joints including one transformation from base;
-% % % % %  
-
-
-%% InterpolateWaypointRadians
-% Given a set of waypoints, finely intepolate them
-function qMatrix = InterpolateWaypointRadians(waypointRadians,maxStepRadians)
-if nargin < 2
-    maxStepRadians = deg2rad(1);
 end
 
-qMatrix = [];
-for i = 1: size(waypointRadians,1)-1
-    qMatrix = [qMatrix ; FineInterpolation(waypointRadians(i,:),waypointRadians(i+1,:),maxStepRadians)]; %#ok<AGROW>
-end
+%% IsIntersectionPointInsideTriangle
+% Given a point which is known to be on the same plane as the triangle
+% determine if the point is 
+% inside (result == 1) or 
+% outside a triangle (result ==0 )
+function result = IsIntersectionPointInsideTriangle(intersectP,triangleVerts)
+
+u = triangleVerts(2,:) - triangleVerts(1,:);
+v = triangleVerts(3,:) - triangleVerts(1,:);
+
+uu = dot(u,u);
+uv = dot(u,v);
+vv = dot(v,v);
+
+w = intersectP - triangleVerts(1,:);
+wu = dot(w,u);
+wv = dot(w,v);
+
+D = uv * uv - uu * vv;
+
+% Get and test parametric coords (s and t)
+s = (uv * wv - vv * wu) / D;
+if (s < 0.0 || s > 1.0)        % intersectP is outside Triangle
+    result = 0;
+    return;
 end
 
+t = (uv * wu - uu * wv) / D;
+if (t < 0.0 || (s + t) > 1.0)  % intersectP is outside Triangle
+    result = 0;
+    return;
+end
+
+result = 1;                      % intersectP is in Triangle
+end
 
 %% IsCollision
 % This is based upon the output of questions 2.5 and 2.6
@@ -201,6 +139,55 @@ for qIndex = 1:size(qMatrix,1)
             end
         end    
     end
+end
+end
+
+%% GetLinkPoses
+% q - robot joint angles
+% robot -  seriallink robot model
+% transforms - list of transforms
+function [ transforms ] = GetLinkPoses( q, robot)
+
+links = robot.links;
+transforms = zeros(4, 4, length(links) + 1);
+transforms(:,:,1) = robot.base;
+
+for i = 1:length(links)
+    L = links(1,i);
+    
+    current_transform = transforms(:,:, i);
+    
+    current_transform = current_transform * trotz(q(1,i) + L.offset) * ...
+    transl(0,0, L.d) * transl(L.a,0,0) * trotx(L.alpha);
+    transforms(:,:,i + 1) = current_transform;
+end
+end
+
+%% FineInterpolation
+% Use results from Q2.6 to keep calling jtraj until all step sizes are
+% smaller than a given max steps size
+function qMatrix = FineInterpolation(q1,q2,maxStepRadians)
+if nargin < 3
+    maxStepRadians = deg2rad(1);
+end
+    
+steps = 2;
+while ~isempty(find(maxStepRadians < abs(diff(jtraj(q1,q2,steps))),1))
+    steps = steps + 1;
+end
+qMatrix = jtraj(q1,q2,steps);
+end
+
+%% InterpolateWaypointRadians
+% Given a set of waypoints, finely intepolate them
+function qMatrix = InterpolateWaypointRadians(waypointRadians,maxStepRadians)
+if nargin < 2
+    maxStepRadians = deg2rad(1);
+end
+
+qMatrix = [];
+for i = 1: size(waypointRadians,1)-1
+    qMatrix = [qMatrix ; FineInterpolation(waypointRadians(i,:),waypointRadians(i+1,:),maxStepRadians)]; %#ok<AGROW>
 end
 end
 
