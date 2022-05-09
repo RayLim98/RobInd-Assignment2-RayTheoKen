84d7b208501f29cb46bbe83efaf6ad165f340a74
Raymond Lim <rlim8868@gmail.com>
2022-05-09 21:04:46 +1000

    waypoint to cup added

--- Test/ROBOT_ENV_Test_Function.m
+++ Test/ROBOT_ENV_Test_Function.m
@@ -16,20 +16,22 @@ plot3(p3(1), p3(2), p3(3),'*','Color', 'g' )
 p4 = [0.55, 1.85, 1];
 plot3(p4(1), p4(2), p4(3),'*','Color', 'y' )
 
-p5 = [0, 2.8, 1];
+p5 = [0, 2.6, 1];
 plot3(p5(1), p5(2), p5(3),'*','Color', 'c' )
 
-%% Trajectory to points from default/home position
+%% Reaching for Cup position
 %These are manually configure
-qC = sBot.model.getpos;
-qBlueF = [ 0.3777 0.7294 0.3277 1.6391 -0.0242 2.6180 -0.5993];
-qRedF = [ 1.4336   -0.4312    0.2666    0.8083   -0.0242    2.6180   -0.5993];
-qCup = sBot.model.ikcon()
-% Rot = [ -0.0002, 1, 0.0002; 0.0008,   -0.0002,    1; 1,    0.0002,   -0.0008 ];
+sBot.model.plot3d(sBot.qOp)
+% qC = sBot.model.getpos;
+% qBlueF = [ 0.3777 0.7294 0.3277 1.6391 -0.0242 2.6180 -0.5993];
+% qRedF = [ 1.4336   -0.4312    0.2666    0.8083   -0.0242    2.6180   -0.5993];
+% qCup = [1.0385   -1.9437         0    1.5429         0    2.0320         0];
 
-qM = sBot.genTrajRMRC(p5);
+% trCup = transl(0,2.8,1) *  troty(pi/2)
+% qM = sBot.genTraj(trCup);
+% qM = jtraj(sBot.model.getpos, qCup,50);
 
-% qM = jtraj(sBot.model.getpos(), qCup, 50);
-
+qM = sBot.GoToCupTrajectory;
 sBot.model.plot3d(qM)
+
 %% RMRC
